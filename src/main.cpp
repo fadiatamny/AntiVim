3c33136bd192bd816629c2749f5a642c4121c5a3
Fadi Atamny <fadiatamny@gmail.com>
2023-08-04 21:23:25 +0300

    cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include "lib/Renderer/Renderer.hpp"
-#include <thread>
-#include <chrono>
 
 const char *title = "AntiVim";
 
 int main(int argc, char *argv[])
 {
+    std::string filename;
+    if (argc > 1)
+    {
+        filename = argv[1];
+    }
+
     Renderer renderer;
     renderer.init(800, 600, title);
+    if (filename.length()) {
+        renderer.loadFile(filename);
+    }
     renderer.run();
     return 0;
 }
