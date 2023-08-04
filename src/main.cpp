42578bc663dfd82667f5af0021ed92a0be8cd9e2
Fadi Atamny <fadiatamny@gmail.com>
2023-08-04 04:31:06 +0300

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,10 @@
 
 const char *title = "AntiVim";
 
-void runThreadsApproach(Renderer &renderer) {
-    std::thread thread1([&renderer]()
-                        { renderer.run(); });
-
-    std::thread thread2([&renderer]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        renderer.renderText("Test Text", { 10, 10 });
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-        renderer.renderText("Test Text 2", { 10, 10 }, 0xFF000066, 5.f);
-    });
-
-    
-    thread1.join();
-    thread2.join();
-}
-
-void runSingleThreadApproach(Renderer &renderer) {
-    renderer.run();
-}
-
 int main(int argc, char *argv[])
 {
     Renderer renderer;
     renderer.init(800, 600, title);
-
-    runSingleThreadApproach(renderer);
-    // runThreadsApproach(renderer);
-
+    renderer.run();
     return 0;
 }
